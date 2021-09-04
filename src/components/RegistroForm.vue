30d771246a9897a87b92ab91c78d7650dc378bee
DESKTOP-D94GMU7\mike <maic76@gmail.com>
2021-09-04 12:52:04 -0500

    Agregando aviso de privacidad

--- src/components/RegistroForm.vue
+++ src/components/RegistroForm.vue
@@ -27,14 +27,72 @@
                           	<v-text-field block v-model="verify" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, passwordMatch]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Confirm Password" counter @click:append="show1 = !show1"></v-text-field>
                         	</v-col>
                          <v-spacer></v-spacer>
-                         <v-col class="d-flex ml-auto" cols="12" sm="3" xsm="12">
+                         <v-col class="d-flex justify-space-between mb-6  " cols="12" sm="12" xsm="12">
                              <v-btn x-large block :disabled="!valid" color="success" @click="validate">Registrar</v-btn>
-                         </v-col>					 
+                         </v-col>
+                         
+                         	 <v-col class=" justify-space-between mb-0 mt-0 " cols="12" sm="12" xsm="12">
+                         	<template>
+							  <div class="text-center">
+							  
+							    <v-dialog
+							      v-model="dialog"
+							      width="500"
+							    >
+							      <template v-slot:activator="{ on, attrs }">
+							        <v-btn
+							          color="blue lighten-1"
+							          dark
+							          v-bind="attrs"
+							          v-on="on"
+							          outlined
+							          class="mx-auto mt-0"
+							        >
+							          Aviso de privacidad
+							        </v-btn>
+							        	<p class="font-italic mt-1" >Al crear su cuenta usted acepta el acuerdo de privacidad de LANIA. </p>
+							      </template>
+
+							      <v-card>
+							        <v-card-title class="text-h5 grey lighten-2">
+							          Aviso de privacidad
+							        </v-card-title>
+
+							        <v-card-text>
+							          Usted expresamente acepta Política de	Privacidad	al	registrarse, accede	a,	navegar	por,	
+										o utilizar nuestros productos, contenido, funcionalidades,	tecnología	o	funciones	disponibles	en	redes	sociales	y	nuestro	sitio www.lania.edu.mx/sira
+							        </v-card-text>
+
+							        <v-divider></v-divider>
+
+							        <v-card-actions>
+							          <v-spacer></v-spacer>
+							          <v-btn
+							            color="primary"
+							            text							        
+							            @click="dialog = false"
+							          >
+							            Acepto
+							          </v-btn>
+							        </v-card-actions>
+							      </v-card>
+							    </v-dialog>
+		  						</div>
+							</template>
+                         </v-col>
+                        
+
+
+
+
 				</v-row>	
 				</v-form>
 				
 			</v-card-text>
 		</v-card>
+
+		
+
 	</v-main>
 </template>
 
@@ -45,6 +103,7 @@
  export default {
  
 	   data: () => ({
+	   			dialog: false,
 		      	valid: true,
 		    	nombre: "",		      
 		        apellido:"", 
